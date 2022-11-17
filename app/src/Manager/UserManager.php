1aeae4e294a19c4942cd2af0b99d18a086dc0961
gaoubak <kader@spin-interactive.com>
2022-11-17 15:11:28 +0100

    little change for the login

--- app/src/Manager/UserManager.php
+++ app/src/Manager/UserManager.php
@@ -24,4 +24,54 @@ class UserManager extends BaseManager
 
         return $users;
     }
+
+    public function UserNameExist($name, $email)
+    {
+        $query = $this->pdo->query("SELECT * FROM users WHERE username = ? OR email = ?;");
+        $query->execute([$name, $email]);
+        $data = $query->fetch(PDO::FETCH_ASSOC);
+
+        if ($data) {
+            return $data;
+        } else {
+            return false;
+        }
+    }
+
+    function pwdMatch($password, $pwdcheck)
+    {
+        if ($password == $pwdcheck) {
+            $result = false;
+        } else {
+            $result = true;
+        }
+    }
+
+    /**
+     * @return User[]
+     */
+    public function login($uid, $pwd): array
+    {
+        $uidExist = $this->UserNameExist($uid, $uid);
+
+        if ($uidExist === false) {
+            header("location: ../login.php?error=wrongLogin");
+            exit();
+        }
+        $pwdHashed = $uidExist['password'];
+        $checkPass = password_verify($pwd, $pwdHashed);
+
+
+        if ($checkPass === false) {
+            header("location: ../login.php?error=wrongLogin");
+            exit();
+        } else if ($checkPass === true) {
+            session_start();
+            $_SESSION["userid"] = $uidExist['id'];
+            $_SESSION["useruid"] = $uidExist['username'];
+            header("location: ../index.php");
+            exit();
+        }
+        return $uidExist;
+    }
 }
