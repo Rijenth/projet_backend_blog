09d4a6fae7ac236ead1dc1cfd69afdb2fe1170f3
Setsu | A.K.A EthLny <60669080+Setsudan@users.noreply.github.com>
2022-11-18 14:41:26 +0100

    blog layout and style added (#3)

--- app/layout/blog/blog.layout.php
+++ app/layout/blog/blog.layout.php
@@ -1,6 +1,6 @@
 <?php
 // Check if user is logged in
-if (!isset($_SESSION['user'])) {
+/* if (!isset($_SESSION['user'])) {
     header("Location: /login");
     exit();
 }
@@ -8,7 +8,7 @@ if (!isset($_SESSION['user'])) {
 if (!isset($_COOKIE['user'])) {
     header("Location: /login");
     exit();
-}
+} */
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -20,57 +20,26 @@ if (!isset($_COOKIE['user'])) {
     <title>
         <? echo $args['pageTitle'] ?>
     </title>
+    <link rel="stylesheet" href="../../styles/globals.css" />
+
 </head>
 
 <body>
 
     <main id="blog_page">
-        <h2>
-            <? echo $args['pageTitle'] ?>
-        </h2>
         <div class="blog_posts-wrapper">
-
+            <div class="blog_post">
+                <h3 class="blog_post-title">
+                    Post test
+                </h3>
+                <p class="blog_post-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor at fuga ut
+                    consectetur magni nesciunt consequatur cum amet obcaecati vel facilis quisquam exercitationem, illum
+                    incidunt delectus ducimus impedit! Quo, consequatur!</p>
+            </div>
         </div>
     </main>
     <script>
     const current_user = <?php echo json_encode($_SESSION['user']) ?>;
-    fetch('/api/posts')
-        .then(response => response.json())
-        .then(posts => {
-            for (post of posts) {
-                const postElement = document.createElement('div');
-                postElement.classList.add('blog_post');
-                if (post.user_id == current_user.id) {
-                    postElement.classList.add('blog_post--current-user');
-                }
-                postElement.innerHTML = `
-                    <div class="blog_post-header">
-                        <h3>${post.title}</h3>
-                        <p>${post.content}</p>
-                    </div>
-                    `;
-                // If the post is from the current user, add a delete button
-                if (post.user_id == current_user.id) {
-                    const deleteButton = document.createElement('button');
-                    deleteButton.classList.add('blog_post-delete');
-                    deleteButton.innerHTML = 'Delete';
-                    deleteButton.addEventListener('click', () => {
-                        fetch(`/api/post/delete/${post.id}`, {
-                                method: 'DELETE'
-                            })
-                            .then(response => response.json())
-                            .then(data => {
-                                if (data.success) {
-                                    // Remove the post from the DOM
-                                    postElement.remove();
-                                }
-                            })
-                    })
-                    postElement.appendChild(deleteButton);
-                }
-                document.querySelector('.blog_posts-wrapper').appendChild(postElement);
-            }
-        })
     </script>
 </body>
 
