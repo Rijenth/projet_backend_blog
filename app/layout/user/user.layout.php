ceefb3f387c42af6594820eabebe28f470acf7c6
Setsudan <cryptiareta@gmail.com>
2022-11-18 13:48:59 +0100

    modified user page

--- app/layout/user/user.layout.php
+++ app/layout/user/user.layout.php
@@ -22,24 +22,30 @@ if (!isset($_COOKIE['user'])) {
     <title>
         <? echo $args['pageTitle'] ?>
     </title>
+    <link rel="stylesheet" href="../../styles/globals.css" />
 </head>
 
 <body>
     <main id="user-page">
         <h1>User profile</h1>
+        <div id="user-card">
+
+        </div>
+        <a href="/">Home</a>
     </main>
     <script>
-    // test the fetch to /api/user/{user_id}
-
-    const userId = <?php echo $args['userId'] ?>;
-    const url = `/api/user/${userId}`;
-
     let notFound = false;
     let username;
     let email;
     let firstName;
     let lastName;
     let gender;
+
+    let userId = <?php echo $args['userId'] ?>;
+    let url = `/api/user/${userId}`;
+
+
+
 
     // if json parse fails, it means the user was not found
     fetch(url)
@@ -65,30 +71,30 @@ if (!isset($_COOKIE['user'])) {
         });
 
     function createProfile() {
-        const userPage = document.getElementById("user-page");
+        const userPage = document.getElementById("user-card");
 
         const profile = document.createElement("div");
         profile.classList.add("profile");
 
-        const profileUsername = document.createElement("h2");
+        const profileUsername = document.createElement("p");
         profileUsername.classList.add("profile-username");
-        profileUsername.innerText = username;
+        profileUsername.innerText = 'Username: ' + username;
 
         const profileEmail = document.createElement("p");
         profileEmail.classList.add("profile-email");
-        profileEmail.innerText = email;
+        profileEmail.innerText = 'Email: ' + email;
 
         const profileFirstName = document.createElement("p");
         profileFirstName.classList.add("profile-first-name");
-        profileFirstName.innerText = firstName;
+        profileFirstName.innerText = 'First name: ' + firstName;
 
         const profileLastName = document.createElement("p");
         profileLastName.classList.add("profile-last-name");
-        profileLastName.innerText = lastName;
+        profileLastName.innerText = 'Last name: ' + lastName;
 
         const profileGender = document.createElement("p");
         profileGender.classList.add("profile-gender");
-        profileGender.innerText = gender;
+        profileGender.innerText = 'Gender: ' + gender;
 
         profile.appendChild(profileUsername);
         profile.appendChild(profileEmail);
