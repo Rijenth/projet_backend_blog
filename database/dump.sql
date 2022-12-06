cbf7a1dd4ee9321b750da760c0e04f90351fb13f
A_Rijenth <rijenth93100@laposte.net>
2022-12-06 19:04:07 +0100

    Update dump.sql
    Erreur de clés étrangères invalides à cause de l'ordre de création des tables

--- database/dump.sql
+++ database/dump.sql
@@ -12,19 +12,20 @@ SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
 
-DROP TABLE IF EXISTS `Comments`;
+DROP TABLE IF EXISTS `User`;
 
 
-CREATE TABLE `Comments` (
+CREATE TABLE `User` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `content` text NOT NULL,
-    `user_id` int(11) NOT NULL,
-    `post_id` int(11) NOT NULL,
-    PRIMARY KEY (`id`), KEY `user_id` (`user_id`),
-    KEY `post_id` (`post_id`),
-    CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE,
-    CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `Post` (`id`) ON DELETE CASCADE
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    `username` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `email` varchar(255) NOT NULL,
+    `firstName` varchar(255) DEFAULT NULL,
+    `lastName` varchar(255) DEFAULT NULL,
+    `gender` varchar(10) DEFAULT NULL,
+    `roles` text NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
 DROP TABLE IF EXISTS `Post`;
@@ -43,6 +44,21 @@ CREATE TABLE `Post` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
+DROP TABLE IF EXISTS `Comments`;
+
+
+CREATE TABLE `Comments` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `content` text NOT NULL,
+    `user_id` int(11) NOT NULL,
+    `post_id` int(11) NOT NULL,
+    PRIMARY KEY (`id`), KEY `user_id` (`user_id`),
+    KEY `post_id` (`post_id`),
+    CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `User` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `Post` (`id`) ON DELETE CASCADE
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 DROP TABLE IF EXISTS `sub_comments`;
 
 
@@ -58,20 +74,5 @@ CREATE TABLE `sub_comments` (
     CONSTRAINT `sub_comments_ibfk_2` FOREIGN KEY (`comments_id`) REFERENCES `Comments` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-DROP TABLE IF EXISTS `User`;
-
-
-CREATE TABLE `User` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
-    `username` varchar(255) NOT NULL,
-    `password` varchar(255) NOT NULL,
-    `email` varchar(255) NOT NULL,
-    `firstName` varchar(255) DEFAULT NULL,
-    `lastName` varchar(255) DEFAULT NULL,
-    `gender` varchar(10) DEFAULT NULL,
-    `roles` text NOT NULL,
-    PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- 2022-11-15 20:28:58
